5611bb742b5a2eee94e28022efa452e15ece7791
Muneeb Ur Rehman <muneeburrehman250@gmail.com>
2023-04-11 18:12:55 +0500

    small styling changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -135,19 +135,20 @@ const Home = () => {
     return (
         <div className="home">
             <Header />
-            <div onClick={handleAddress} className="mockup-code my-5 w-1/3 mx-auto  lg:overflow-visible overflow-hidden bg-indigo-500 hover:bg-indigo-600 text-sm lg:text-base cursor-pointer shadow-xl">
+            <div onClick={handleAddress} className="mockup-code my-5 w-1/3 mx-auto  lg:overflow-visible overflow-hidden bg-indigo-500 hover:bg-indigo-600 text-sm lg:text-base cursor-pointer shadow-xl text-center">
                 <div>
-                    <pre data-prefix="//"><code >Click to use this address 👇</code></pre>
-                    <pre><code>0xBAa4b7858....05f2017aFea19A</code></pre>
+                    <pre><code >Click to use this address</code></pre>
+                    <pre><code>0xBAa4b7858....2017aFea19A</code></pre>
                 </div>
 
             </div>
-            <h1 className='text-center text-[2.7rem] mt-10 font-black text-gradient__gray sm:text-7xl drop-shadow-xl '>NFT GALLERY</h1>
+            <h1 className='text-center text-[2.8rem] mt-10 font-black text-gradient sm:text-7xl drop-shadow-xl '>NFT GALLERY</h1>
             <div className="flex flex-col items-center justify-center py-8 gap-y-3">
                 <div className="flex flex-col w-full justify-center items-center gap-y-2">
-                    <input className='blue-glassmorphism my-2 sm:my-0' value={wallet} disabled={fetchForCollection} onChange={(e) => setWalletAddress(e.target.value)} type="text" placeholder="Add your wallet address"></input>
-                    <input className='blue-glassmorphism my-2 sm:my-0' value={collection} onChange={(e) => setCollectionAddress(e.target.value)} type="text" placeholder="Add the collection address"></input>
-                    <label className="text-white"><input type={"checkbox"} className="mr-2 my-3 sm:my-0" value={fetchForCollection} onChange={(e) => setFetchForCollection(e.target.checked)}></input>Fetch for collection</label>
+                    <input className='blue-glassmorphism my-2 sm:my-0 placeholder:text-slate-400' value={wallet} disabled={fetchForCollection} onChange={(e) => setWalletAddress(e.target.value)} type="text" placeholder="Add your wallet address"></input>
+                    <input className='blue-glassmorphism my-2 sm:my-0 placeholder:text-slate-400' value={collection} onChange={(e) => setCollectionAddress(e.target.value)} type="text" placeholder="Add the collection address"></input>
+                    <label className="text-white "><input type={"checkbox"} className="mr-2 my-3 sm:my-0" value={fetchForCollection} onChange={(e) => setFetchForCollection(e.target.checked)}></input>Fetch for collection</label>
+
                     <button className='text-white my-btn shadow-xl' onClick={handleFetch}>Let's go!</button>
                 </div>
             </div>
